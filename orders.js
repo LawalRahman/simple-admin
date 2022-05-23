a0d717fec1f9d47ebec91222edc71abe2b1cb19d
Lawal Rahman <rhaman.lawal@tdtechgh.com>
2022-05-24 02:06:09 +0300

    complete project

---
+++ orders.js
@@ -0,0 +1,56 @@
+const orders = [
+  {
+    productName: "Foldable Mini Drone",
+    ProductNumber: "125635",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    ProductNumber: "125635",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    ProductNumber: "125635",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    ProductNumber: "125635",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    ProductNumber: "125635",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    ProductNumber: "125635",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    ProductNumber: "125635",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    ProductNumber: "125635",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Foldable Mini Drone",
+    ProductNumber: "125635",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
